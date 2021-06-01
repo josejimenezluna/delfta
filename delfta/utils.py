3e2184904bc6385086310bb1dcff5411ca828494
Jose Jimenez <hawk31@gmail.com>
2021-06-01 13:35:04 +0200

    xtb binary

--- delfta/utils.py
+++ delfta/utils.py
@@ -1,8 +1,7 @@
-from delfta.xtb import XTB_BINARY
 import os
+
 
 DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
 MODEL_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "models")
 XTB_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "xtb")
-
-XTB_BINARY = os.path.join(XTB_PATH, "")
+XTB_BINARY = os.path.join(XTB_PATH, "xtb-6.3.1", "bin", "xtb")
