e3df4f428a5174ceda912e2549a5b54eae4567dd
cisert <cisert@ethz.ch>
2021-06-29 01:21:56 +0200

    Error propagation added to calculator.

--- delfta/calculator.py
+++ delfta/calculator.py
@@ -3,7 +3,6 @@ import os
 import pickle
 import textwrap
 import types
-from typing import ValuesView
 
 import numpy as np
 import torch
@@ -19,10 +18,11 @@ from delfta.net_utils import (
     DeltaDataset,
 )
 import openbabel
-from delfta.utils import LOGGER, MODEL_PATH
+from delfta.utils import LOGGER, MODEL_PATH, ELEM_TO_ATOMNUM
 from delfta.xtb import run_xtb_calc
 
 _ALLTASKS = ["E_form", "E_homo", "E_lumo", "E_gap", "dipole", "charges"]
+PLACEHOLDER = float("NaN")
 
 
 class DelftaCalculator:
@@ -30,8 +30,8 @@ class DelftaCalculator:
         self,
         tasks="all",
         delta=True,
-        force3d=False,
-        addh=False,
+        force3d=True,
+        addh=True,
         xtbopt=False,
         verbose=True,
         progress=True,
@@ -94,11 +94,14 @@ class DelftaCalculator:
 
         self.models = list(set(self.models))
 
+    def _molcheck(self, mol):
+        return isinstance(mol, openbabel.pybel.Molecule) and len(mol.atoms) > 0
+
     def _3dcheck(self, mol):
         """Checks whether `mol` has 3d coordinates assigned. If
         `self.force3d=True` these will be computed for
         those lacking them using the MMFF94 force-field as
-        available on pybel.
+        available in pybel.
 
         Parameters
         ----------
@@ -175,16 +178,6 @@ class DelftaCalculator:
         else:
             return True
 
-    def _validate_mols(self, mols):
-        if len(mols) == 0:
-            raise ValueError("No molecules provided.")
-        correct_types = [isinstance(mol, openbabel.pybel.Molecule) for mol in mols]
-        not_empty = [len(mol.atoms) > 0 for mol in mols]
-        valid = [(a and b) for a, b in zip(correct_types, not_empty)]
-        if not all(valid):
-            idx = [i for i, elem in enumerate(valid) if not elem]
-            raise ValueError(f"Invalid molecules at idx {idx}.")
-
     def _preprocess(self, mols):
         """Performs a series of preprocessing checks on a list of molecules `mols`,
         including 3d-conformation existence, validity of atom types, neutral charge
@@ -197,96 +190,99 @@ class DelftaCalculator:
 
         Returns
         -------
-        [pybel.Molecule]
-            A list of processed OEChem molecule objects
+        ([pybel.Molecule], [int])
+            A list of processed OEChem molecule objects; and indices of molecules that cannot be processed.
         
         """
-        self._validate_mols(mols)
-        idx_no3d = []
+        if len(mols) == 0:
+            raise ValueError("No molecules provided.")
+
+        idx_non_valid_mols = []
         idx_non_valid_atypes = []
         idx_charged = []
+        idx_no3d = []
         idx_noh = []
+        fatal = []
 
         for idx, mol in enumerate(mols):
-            has_3d = self._3dcheck(mol)
-            if not has_3d:
-                idx_no3d.append(idx)
+            valid_mol = self._molcheck(mol)
+            if not valid_mol:
+                idx_non_valid_mols.append(idx)
+                fatal.append(idx)
+                continue  # no need to check further
 
             is_atype_valid = self._atomtypecheck(mol)
             if not is_atype_valid:
                 idx_non_valid_atypes.append(idx)
+                fatal.append(idx)
+                continue  # no need to check further
 
             is_charged = self._chargecheck(mol)
             if is_charged:
                 idx_charged.append(idx)
+                fatal.append(idx)
+                continue  # no need to check further
+
+            has_3d = self._3dcheck(mol)
+            if not has_3d:
+                idx_no3d.append(idx)
 
             has_h = self._hydrogencheck(mol)
             if not has_h:
                 idx_noh.append(idx)
 
+            if not has_3d and not has_h and not self.addh:
+                fatal.append(idx)
+                # cannot assign 3D geometry without assigning hydrogens
+
+        if idx_non_valid_mols:
+            LOGGER.warning(
+                f"""Invalid molecules at position(s) {idx_non_valid_mols}."""
+            )
+
         if idx_no3d:
             if self.force3d:
                 if self.verbose:
                     LOGGER.info(
-                        f"Assigned MMFF94 coordinates to molecules with idx. {idx_no3d}"
+                        f"""Assigned MMFF94 coordinates to molecules with idx. {idx_no3d}"""
                     )
 
             else:
-                raise ValueError(
-                    textwrap.fill(
-                        textwrap.dedent(
-                            f"""
-                Molecules at position {idx_no3d} have no 3D conformations available.
-                Either provide a mol with one or re-run calculator with `force3D=True`.
-                """
-                        )
-                    )
+                LOGGER.warning(
+                    f"""Molecules at position {idx_no3d} have no 3D conformations available. 
+                        Either provide a mol with one or re-run calculator with `force3D=True`.
+                    """
                 )
 
         if idx_non_valid_atypes:
-            raise ValueError(
-                textwrap.fill(
-                    textwrap.dedent(
-                        f"""
-                            Found non-supported atomic no. in molecules
-                            at position {idx_non_valid_atypes}. This application currently supports only
-                            the atom types used in the QMugs database, namely those with
-                            the following atomic numbers {list(QMUGS_ATOM_DICT.keys())}.
-                            """
-                    )
-                )
+            LOGGER.warning(
+                f"""Found non-supported atomic no. in molecules at position 
+                    {idx_non_valid_atypes}. This application currently supports 
+                    only the atom types used in the QMugs database, namely 
+                    {list(ELEM_TO_ATOMNUM.keys())}.
+                """
             )
         if idx_charged:
-            raise ValueError(
-                textwrap.fill(
-                    textwrap.dedent(
-                        f"""
-                        Found molecules with a non-zero atomic formal charge at 
-                        positions {idx_charged}. This application currently does not support
-                        prediction for charged molecules.
-                        """
-                    )
-                )
+            LOGGER.warning(
+                f"""Found molecules with a non-zero formal charge at 
+                    positions {idx_charged}. This application currently does not support
+                    prediction for charged molecules.
+                """
             )
 
         if idx_noh:
             if self.addh:
                 LOGGER.info(
-                    f"Added hydrogens for non-protonated molecules at position {idx_noh}"
+                    f"""Added hydrogens for non-protonated molecules at position {idx_noh}"""
                 )
             else:
-                raise ValueError(
-                    textwrap.fill(
-                        textwrap.dedent(
-                            f"""
-                            No hydrogens present for molecules at position {idx_noh}. Please add
-                            them manually or re-run the calculator with argument `addh=True`.
-                            """
-                        )
-                    )
+                LOGGER.warning(
+                    f"""No hydrogens present for molecules at position {idx_noh}. Please 
+                        add them manually or re-run the calculator with argument `addh=True`.
+                    """
                 )
-
-        return mols
+        good_mols = [mol for idx, mol in enumerate(mols) if idx not in fatal]
+        return good_mols, fatal
 
     def _get_preds(self, loader, model):
         """Returns predictions for the data contained in `loader` of a
@@ -433,7 +429,7 @@ class DelftaCalculator:
             Requested DFT-predicted properties.
         """
         if isinstance(input_, list):
-            mols = self._preprocess(input_)
+            mols, fatal = self._preprocess(input_)  # TODO --> add error propagation
 
         elif isinstance(input_, types.GeneratorType):
             return self._predict_batch(input_, batch_size)
@@ -442,6 +438,9 @@ class DelftaCalculator:
             raise ValueError(
                 f"Invalid input. Expected list or generator, but got {type(input_)}."
             )
+
+        if self.delta:
+            xtb_props = self._get_xtb_props(mols)  # TODO --> add error propagation
 
         data = DeltaDataset(mols)
         loader = DataLoader(data, batch_size=batch_size, shuffle=False)
@@ -496,8 +495,6 @@ class DelftaCalculator:
                 preds_filtered["charges"] = preds[model_name]
 
         if self.delta:
-            xtb_props = self._get_xtb_props(mols)
-
             for prop, delta_arr in preds_filtered.items():
                 if prop == "charges":
                     preds_filtered[prop] = [
@@ -508,7 +505,22 @@ class DelftaCalculator:
                     preds_filtered[prop] = delta_arr + np.array(
                         xtb_props[prop], dtype=np.float32
                     )
-        return preds_filtered
+
+        # insert placeholder values where errors occurred
+        preds_final = {}
+        for key, val in preds_filtered.items():
+            val = val.tolist() if key != "charges" else val
+            temp = []
+            for idx in range(len(input_)):
+                if idx in fatal:
+                    if key == "charges":
+                        temp.append(np.array(PLACEHOLDER))
+                    else:
+                        temp.append(PLACEHOLDER)
+                else:
+                    temp.append(val.pop())
+            preds_final[key] = temp
+        return preds_final
 
 
 if __name__ == "__main__":
@@ -516,8 +528,13 @@ if __name__ == "__main__":
     from delfta.utils import DATA_PATH
     from openbabel.pybel import readfile
 
-    mol_files = glob.glob(os.path.join(DATA_PATH, "test_data", "example_files_1.sdf"))
-    mols = [next(readfile("sdf", mol_file)) for mol_file in mol_files]
+    mol_files = glob.glob(os.path.join(DATA_PATH, "incorrect_mols", "*.sdf"))
+    mols = []
+    for mol_file in mol_files:
+        try:
+            mols.append(next(readfile("sdf", mol_file)))
+        except:
+            print(os.path.basename(mol_file))
     calc_delta = DelftaCalculator()
     # Verbose passing of arguments. We could've used "all" as well
     predictions_delta = calc_delta.predict(mols, batch_size=32)
