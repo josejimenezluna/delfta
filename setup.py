1bca0c0646f0dd30445f60c07c06d50e1e668217
Jose Jimenez-Luna <hawk31@gmail.com>
2021-09-01 09:54:15 +0200

    automatic conda package upon release workflow

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
     
 setup(name='delfta',
-    version="0.5",
+    version="0.5.1",
     description='delfta',
     classifiers=[
     'Development Status :: 2 - Pre-Alpha',
