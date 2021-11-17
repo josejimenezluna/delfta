14174b55d229efc040cd58af203e231ec46ab6f2
Jose Jimenez-Luna <hawk31@gmail.com>
2021-11-17 11:19:01 +0100

    sum pooled eform models

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
     
 setup(name='delfta',
-    version="0.6.0",
+    version="0.7.0",
     description='delfta',
     classifiers=[
     'Development Status :: 2 - Pre-Alpha',
